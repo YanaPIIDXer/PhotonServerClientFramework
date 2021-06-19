644598866bcbf031d808f1514f362ba2ea007f6e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-19 16:24:10 +0900

    IPhotonPeerListenerを実装

--- Assets/Plugins/PhotonServerClientFramework/Scripts/PhotonConnection.cs
+++ Assets/Plugins/PhotonServerClientFramework/Scripts/PhotonConnection.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ExitGames.Client.Photon;
 
 namespace PhotonServerClient
 {
     /// <summary>
     /// 接続クラス
     /// </summary>
-    public class PhotonConnection : MonoBehaviour
+    public class PhotonConnection : MonoBehaviour, IPhotonPeerListener
     {
         /// <summary>
         /// 生成
@@ -20,6 +21,39 @@ namespace PhotonServerClient
 
             var connection = obj.AddComponent<PhotonConnection>();
             return connection;
+        }
+
+        public void DebugReturn(DebugLevel level, string message)
+        {
+            switch (level)
+            {
+                case DebugLevel.WARNING:
+
+                    Debug.LogWarning(string.Format("[{0}]: {1}", level.ToString(), message));
+                    break;
+
+                case DebugLevel.ERROR:
+
+                    Debug.LogError(string.Format("[{0}]: {1}", level.ToString(), message));
+                    break;
+
+                default:
+
+                    Debug.Log(string.Format("[{0}]: {1}", level.ToString(), message));
+                    break;
+            }
+        }
+
+        public void OnEvent(EventData eventData)
+        {
+        }
+
+        public void OnOperationResponse(OperationResponse operationResponse)
+        {
+        }
+
+        public void OnStatusChanged(StatusCode statusCode)
+        {
         }
     }
 }
