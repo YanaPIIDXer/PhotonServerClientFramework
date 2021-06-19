50172f90c8d84db9f9a7ee4e265c19db9279e7e8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-19 23:22:03 +0900

    Request/Responseのテスト

--- Assets/Example/Scripts/TestClient.cs
+++ Assets/Example/Scripts/TestClient.cs
@@ -6,6 +6,8 @@ using UniRx;
 using PhotonServerClient;
 using System;
 using ExitGames.Client.Photon;
+using Photon.MmoDemo.Common;
+using Cysharp.Threading.Tasks;
 
 /// <summary>
 /// テスト用クライアント
@@ -24,16 +26,30 @@ public class TestClient : MonoBehaviour
 
     void Awake()
     {
+        PhotonPeer.RegisterType(typeof(Vector), (byte)Photon.MmoDemo.Common.Protocol.CustomTypeCodes.Vector, Photon.MmoDemo.Common.Protocol.SerializeVector, Photon.MmoDemo.Common.Protocol.DeserializeVector);
+        PhotonPeer.RegisterType(typeof(BoundingBox), (byte)Photon.MmoDemo.Common.Protocol.CustomTypeCodes.BoundingBox, Photon.MmoDemo.Common.Protocol.SerializeBoundingBox, Photon.MmoDemo.Common.Protocol.DeserializeBoundingBox);
+
         button = GetComponent<Button>();
         button.OnClickAsObservable()
               .Subscribe(async _ =>
               {
                   button.interactable = false;
+                  var token = this.GetCancellationTokenOnDestroy();
 
                   try
                   {
-                      await client.Connect("127.0.0.1:4530", "MMODemo", ConnectionProtocol.Tcp);
+                      await client.Connect("127.0.0.1:4530", "MMODemo", ConnectionProtocol.Tcp, token);
                       Debug.Log("Connection Success!");
+                      await UniTask.Delay(1000);
+
+                      var paramDic = new Dictionary<byte, object>
+                                    {
+                                        { (byte)ParameterCode.WorldName, "World" },
+                                        { (byte)ParameterCode.BoundingBox, new BoundingBox(new Vector(0f, 0f, 0f), new Vector(10f, 10f, 0f)) },
+                                        { (byte)ParameterCode.TileDimensions,  new Vector(1f, 1f, 0f) }
+                                    };
+                      var response = await client.SendOperationRequest((byte)OperationCode.CreateWorld, paramDic, (byte)OperationCode.CreateWorld, token);
+                      Debug.Log(string.Format("Create World Result:{0}", response.ReturnCode));
                   }
                   catch (Exception e)
                   {
