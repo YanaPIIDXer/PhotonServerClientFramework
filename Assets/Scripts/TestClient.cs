95d93924d84ccc6923c5ddd6812bc9e165d509c7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-19 16:13:11 +0900

    テスト用クライアントクラス

---
+++ Assets/Scripts/TestClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using PhotonServerClient;
+
+/// <summary>
+/// テスト用クライアント
+/// </summary>
+public class TestClient : MonoBehaviour
+{
+    /// <summary>
+    /// ボタン
+    /// </summary>
+    private Button button = null;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+}
