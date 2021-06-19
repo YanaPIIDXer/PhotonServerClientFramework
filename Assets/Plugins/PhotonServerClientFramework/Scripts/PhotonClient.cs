5f623c41d0aeabe46dd4c158bb457de425a7cd3b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-19 16:05:08 +0900

    クライアントクラスをとりあえず定義

---
+++ Assets/Plugins/PhotonServerClientFramework/Scripts/PhotonClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PhotonServerClient
+{
+    /// <summary>
+    /// クライアントクラス
+    /// </summary>
+    public class PhotonClient : MonoBehaviour
+    {
+    }
+}
