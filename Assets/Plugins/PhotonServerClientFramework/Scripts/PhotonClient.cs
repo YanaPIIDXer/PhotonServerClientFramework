2d5583dd59835b857c847b9218b229027e227d8c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-19 16:29:23 +0900

    TCPとUDPを併用するケースを考慮して、Singletonではないようにした

--- Assets/Plugins/PhotonServerClientFramework/Scripts/PhotonClient.cs
+++ Assets/Plugins/PhotonServerClientFramework/Scripts/PhotonClient.cs
@@ -15,10 +15,5 @@ namespace PhotonServerClient
         /// </summary>
         private PhotonConnection connection = null;
 
-        #region Singleton
-        public static PhotonClient Instance { get { return instance; } }
-        private static PhotonClient instance = new PhotonClient();
-        private PhotonClient() { }
-        #endregion
     }
 }
